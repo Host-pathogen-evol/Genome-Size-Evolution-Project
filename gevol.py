b138cb17017c1bf8e06254a4b6ff0523ca52bd36
calugo <Carlos.Lugo@tsl.ac.uk>
2014-03-28 17:45:09 +0000

    update

--- gevol.py
+++ gevol.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from pygsl import rng 
+import math as mth
 
 rng=rng.rng()
 #print rng.name()
@@ -7,15 +8,32 @@ i=0
 SEED=987654321-i
 mu=rng.set(SEED)
 
-LH=100 #Host lenght
-LP=10  #Initial Pathogen Length
-LF={}
+LH=100 #Mean number of hosts lengths.
+VHT=50 #SIGMA.
 
-for i in range (0,100):
-	y=1+rng.uniform_int(1000)
-	print y
+#Generate N hosts 
+N=1000 #Number of hosts/jumps
+lhh=[]
+
+#initial pathohen  initial population
+Np=1
+
+for i in range(0,N):	
+	#z=LH+rng.gaussian(VHT) #variable version gaussian
+	z=LH #fixed version	
 	
-	
+print lhh
+print len(lhh)
+raw_input()
 
+#LP=1 #Initial Pathogen units.
+#LF={}
 
-
+#for i in range (0,100):
+#	y=1+rng.uniform_int(1000)
+#	print y
+def newhost(Lh):
+	nhx={}
+	att=[]
+	for i in range (0,Lh):
+		att.append('')
